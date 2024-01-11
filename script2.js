751ed7a8f841237d85a50ee13df9db277c84235f
Jose Navin <shvinjas@gmail.com>
2024-01-12 02:34:17 +1300

    v2

---
+++ script2.js
@@ -0,0 +1,22 @@
+"use strict"
+
+function fetchData() {
+    fetch("https://reqres.in/api/users")
+        .then((response) => response.json())
+        .then((result) => console.log(result.data))
+}
+
+const displaySection = document.getElementById("display-section");
+const disp=document.querySelector(".display")
+function fetchDataAndDisplay() {
+    fetch("https://reqres.in/api/users")
+        .then((response) => response.json())
+        .then((result) => {
+            //displaySection.innerHTML=result.data[0].first_name}
+            result.data.forEach((person)=> {
+        disp.innerHTML = disp.innerHTML + `<li>${person.first_name} ,  ${person.last_name}, <img src= ${person.avatar}> <li>`
+        disp.classList.add("sticky");
+       
+    });
+});
+}
